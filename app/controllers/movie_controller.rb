0460f0f4874f310d7177c300106f41ea763f3a9e
Matt Carson <cattmarson93@gmail.com>
2017-10-05 15:40:38 +0000

    Finished 4.4

---
+++ app/controllers/movie_controller.rb
@@ -0,0 +1,17 @@
+class MoviesController < ApplicationController
+    def index
+        @movies = Movie.all
+    end
+    
+    def show
+      id = params[:id] # retrieve movie ID from URI route
+      @movie = Movie.find(id) # lookup movie by id
+      # will render app/views/movies/show.html.haml by default
+    end
+    # add below all other methods
+private
+
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :description, :release_date)
+  end
+end
